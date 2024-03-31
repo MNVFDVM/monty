563422d294974b8d60d6fe755aa855a0eabaf937
medo <mohammedabdelhamed725@gmail.com>
2024-03-31 15:44:40 -0700

    ghg

--- pus16.c
+++ pus16.c
@@ -1,46 +1,37 @@
 #include "monty.h"
-
 /**
- * f_push - Add node to the stack or queue
- * @head: Pointer to the head of the stack or queue
- * @counter: Line number
- * Return: No return value
- */
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
 void f_push(stack_t **head, unsigned int counter)
 {
-	if (!bus.arg)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
+	int m, i = 0, f = 0;
 
-	int j = 0;
-	int is_negative = 0;
-	if (bus.arg[j] == '-')
+	if (bus.arg)
 	{
-		is_negative = 1;
-		j++;
-	}
-
-	for (; bus.arg[j] != '\0'; j++)
-	{
-		if (!isdigit(bus.arg[j]))
+		if (bus.arg[0] == '-')
+			i++;
+		for (; bus.arg[i] != '\0'; i++)
 		{
-			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			if (bus.arg[i] > 57 || bus.arg[i] < 48)
+				f = 1; }
+		if (f == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	int n = atoi(bus.arg + is_negative);
-
-	if (bus.lifi == 0)
-		addnode(head, n);
+			exit(EXIT_FAILURE); }}
 	else
-		addqueue(head, n);
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	m = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, m);
+	else
+		addqueue(head, m);
 }
